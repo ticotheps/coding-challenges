e65ea4ed4f58eaba314ee10bec167ce21a4b9d18
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-26 20:26:33 -0500

    Updated precourse-work.js file with 'JS: Array Methods' code challenge

--- precourse-work.js
+++ precourse-work.js
@@ -4,6 +4,137 @@
 
 
 
+//*==========* JavaScript: Array Methods *==========*
+
+// The following arrays will be used through the questions:
+const words = [ 'cattywampus', 'snickersnee', 'lollygag', 'pandiculation', 'ratoon' ];
+const numbers = [ 8, 3, 22, 0, -73, 15 ];
+
+// Question 1: 
+// Use forEach to console.log all the words in the words array. Do not write a for loop. 
+function logWords(array){
+  words.forEach(function(item) {
+    console.log(item);
+  });
+}
+
+logWords(words);
+
+
+
+// Question 2: Use forEach to console.log each number in the numbers array divided by two
+// It should console.log: 
+// 4
+// 1.5
+// ...etc. 
+function logHalfNumbers(array){
+  numbers.forEach(function(item) {
+    console.log(item/2);
+  });
+}
+
+logHalfNumbers(numbers);
+
+
+
+// Question 3: Using the getFirstLetter function below as your callback,
+//             use map to create an array contaning the first letter of 
+//             every word from the words array.
+function getFirstLetter(word) {
+  let firstChar = word[0];
+  return firstChar;
+}
+
+const firstLetters = words.map(getFirstLetter);
+console.log(firstLetters);
+
+
+
+// Question 4: Use map to create an array containing the last letter of every word
+//             from the words array. 
+const lastLetters = words.map(function(word) {
+  let numOfChars = word.length;
+  let lastChar = word[numOfChars-1];
+  return lastChar;
+});
+console.log(lastLetters);
+
+
+
+// Question 5: Use map to create an array containing the reversed version of each word
+//             from the word array. For example, the first entry will be 'supmawyttac'.
+const reversedWords = words.map(function(item) {
+  let arrayOfWord = item.split("").reverse();
+  return arrayOfWord.join("");
+});
+
+console.log(reversedWords);
+
+
+
+// Question 6: Use map to create an array containing the absolute value of every number
+//             in the numbers array.
+const absoluteNumbers = numbers.map(function(item) {
+  return Math.abs(item);
+});
+
+console.log(absoluteNumbers);
+
+
+
+// Question 7: Use map to create an array containing each number from the numbers array
+//             rounded to the nearest 10. For example: 10, 0, 20, 0, etc. 
+const roundedToTenNumbers = numbers.map(function(item) {
+  let tenthOfNum = (Math.abs(item))/10;
+
+  if (tenthOfNum < 0.5) {
+    return (Math.floor(item/10))*10;
+  } else if ((tenthOfNum > 0.5) && (tenthOfNum < 1)) {
+      return (Math.ceil(item/10))*10;
+    } else if ((tenthOfNum > 1) && (tenthOfNum < 10)) {
+        return (Math.round(item/10))*10;
+      }
+});
+
+console.log(roundedToTenNumbers);
+
+
+
+// Question 8: Use map to create an array that contains only the words from the words
+//             array containing 's'. All other words should be replaced with null.
+//             For example: 'cattywampus', 'snickersnee', null, etc. 
+const wordsWithS = words.map(function(item) {
+  if (item.includes("s") === true) {
+    return item;
+  } else {
+    return null;
+  }
+});
+
+console.log(wordsWithS);
+
+
+
+// Stretch Questions:
+// The stretch question will use the array method reduce, which was only briefly reviewed 
+// in the lecture. Refer to online documentation for more information on reduce. 
+
+// Question 9: Use reduce to sum up all of the numbers in the numbers array. 
+
+// function sum(currentTotal, nextNumber) {
+
+// }
+
+// const sumTotal = numbers.reduce();
+
+// Question 10: Use reduce to find the word from the words array that is last in the alphabet.
+
+// const alphabeticallyLast = words.reduce(function() {
+
+// });
+
+
+
 //*==========* JavaScript: Higher Order Functions vs. Callbacks *==========*
 
 /* For each example of code please identify 
