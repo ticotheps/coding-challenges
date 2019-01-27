ad1b86c8c3281861d4410c5e1c8221392ebbe037
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-26 19:55:46 -0500

    Updated precourse-work.js file with JS-variables code challenge

--- precourse-work.js
+++ precourse-work.js
@@ -3,6 +3,76 @@
 // Software Engineering/Computer Science program.
 
 
+
+//*==========* JavaScript: Variables *==========*
+
+const noData = null;
+
+// Remove _replaceMe_ and add your own code so that each variable is the type that its name indicates
+const numberVar = 55;
+const stringVar = 'Tico';
+const booleanVar = true;
+const nullVar = noData;
+const undefinedVar = undefined;
+
+// Remove _replaceMe_ and use math operators to assign the following variables. Use Math methods where ever possible. 
+const pi = 3.1415926;
+const piPlusOne = pi+1;
+const piMinusOne = pi-1;
+const piDividedByThree = pi/3;
+const piSquared = Math.pow(pi, 2);
+const piRoundedDown = Math.floor(pi);
+const piRoundedUp = Math.ceil(pi);
+
+// Create an if-else statement that will randomly log "Marco" half of the time and "Polo" the other half.  
+
+if (Math.round(Math.random()) === 1) {
+  console.log('Marco');
+} else {
+  console.log('Polo');
+}
+
+// Remove _replaceMe_ and assign the variables below using x and y. 
+const x = 7.5;
+const y = -15;
+const isXLessThanY = x<y;
+const xMinusY = x-y;
+const yDividedByX = y/x;
+const xAsAString = x.toString();
+const YToThePowerOfX = Math.pow(y, x);
+const yRoundedUp = Math.ceil(y);
+
+//move the console log at the start of the code to greet Evelyn. Do not modify anything else. 
+let mysteryVar = 'start value';
+
+
+const name = 'Evelyn';
+const greeting1 = 'Good morrow';
+const greeting2 = 'Farewell';
+
+if (name.length < Math.random()*10) {
+  mysteryVar = greeting1 + ', ' + name;
+} else {
+  mysteryVar = greeting2 + ', ' + name;
+}
+console.log(mysteryVar);
+mysteryVar = null;
+
+// Stretch Task: 
+// Research the following string methods: includes, indexOf, slice, toUpperCase, toLowerCase
+// Using those methods on 'sampleText', correctly remove _replaceMe_ and assign the following variables.
+const sampleText = "Isn't documentation great!";
+const trueValue = sampleText.includes('great!');
+const falseValue = sampleText.includes('awesome!');
+const three = sampleText.slice(3);
+const nine = sampleText.slice(9);
+const cumen = sampleText.includes('cumen');
+const eat = sampleText.indexOf('eat');
+const DOC = sampleText.slice(6, 9).toUpperCase();
+const is = sampleText.slice(0, 2).toLowerCase();
+
+
+
 //*==========* JavaScript: Classes *==========*
 
 /* === DO NOT ALTER CODE BELOW THIS LINE UNTIL LINE 88 === */
